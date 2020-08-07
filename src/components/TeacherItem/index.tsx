c79e2640780e66d35fc0f71ab336e02c95c04916
AlexandreJareck <alexandrejareck@gmail.com>
2020-08-07 00:50:30 -0300

    finish layout and connect web with api

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,32 +2,50 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem(){
-    return(
+export interface Teacher{
+    id: number,
+    avatar: string,
+    bio: string,
+    cost: number,
+    name: string,
+    subject: string,
+    whatsapp: string
+}
+
+interface TeacherItemPros {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemPros> = ({ teacher }) => {
+
+    function createNewConnection(){
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
+    return (
         <article className="teacher-item">
-        <header>
-            <img src="https://avatars1.githubusercontent.com/u/49285794?s=400&u=ea4ddbb8ff30286480a713882ff8c8064c54f852&v=4" alt="Alexandre Jareck"/>
-            <div>
-                <strong>Alexandre Jareck</strong>
-                <span>Algoritmo</span>
-            </div>
-        </header>
-        <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-        </p>
-        <footer>
-            <p>
-                Preço/Hora
-                <strong>R$80,00</strong>
-                <button type="button">
-                    <img src={whatsappIcon} alt=""/>
-                    Entrar em contato
-                </button>
-            </p>
-        </footer>
-    </article>
+            <header>
+                <img src={teacher.avatar} alt={teacher.name} />
+                <div>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
+                </div>
+            </header>
+            <p>{teacher.bio}</p>
+            <footer>
+                <p>Preço/Hora
+                    <strong>R$ {teacher.cost}</strong>
+                    <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
+                        <img src={whatsappIcon} alt="" />
+                         Entrar em contato
+                    </a>
+                </p>
+            </footer>
+        </article>
     );
 }
 
